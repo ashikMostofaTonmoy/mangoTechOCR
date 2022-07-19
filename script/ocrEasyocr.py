4964ab3ccae8339f0dc05448b629efb630e2ffbd
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-07-19 18:02:11 +0600

    first commit

---
+++ script/ocrEasyocr.py
@@ -0,0 +1,25 @@
+# %%
+import easyocr
+import PIL
+from PIL import ImageDraw
+# %%
+reader = easyocr.Reader(['bn'])
+
+image_name = '../input/mouja map.png'
+
+result = reader.readtext(image_name)
+result
+# %%
+# img = cv2.imread('chinese_tra.jpg')
+# result = reader.readtext(img)
+
+# Draw bounding boxes
+def draw_boxes(image, bounds, color='green', width=2):
+    draw = ImageDraw.Draw(image)
+    for bound in bounds:
+        p0, p1, p2, p3 = bound[0]
+        draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
+    return image
+im = PIL.Image.open(image_name)
+draw_boxes(im, result)
+# %%
